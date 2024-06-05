18c6d06f810f9ebc18400d46024b9fffe4e4ace2
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-06-05 13:41:45 +0530

    rough eks code

--- aws/eks/example/main.tf
+++ aws/eks/example/main.tf
@@ -1,76 +1,161 @@
+
 provider "aws" {
-  region = local.region
+  region = var.aws_region
 }
+
+data "aws_availability_zones" "available" {}
+
 locals {
-  #   name   = "ex-${basename(path.cwd)}"
-  region = var.region
+  cluster_name = "abhi-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
 
 module "vpc" {
-  source = "../"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
 
-  ################################################################################
-  # VPC
-  ################################################################################
+  name                 = "demo-eks-vpc"
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  create_vpc               = var.create_vpc
-  vpc_cidr                 = var.vpc_cidr
-  vpc_tags                 = var.vpc_tags
-  vpc_enable_dns_support   = var.vpc_enable_dns_support
-  vpc_enable_dns_hostnames = var.vpc_enable_dns_hostnames
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
 
-  ################################################################################
-  # VPC Peering
-  ################################################################################
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
 
-  existing_vpc_id   = var.existing_vpc_id
-  existing_rtb      = var.existing_rtb
-  existing_vpc_cidr = var.existing_vpc_cidr
-  vpc_peering_tags  = var.vpc_peering_tags
-  create_vpc_peering = var.create_vpc_peering
-
-  ################################################################################
-  # Publiс Subnets
-  ################################################################################
-
-  public_subnets_cidr            = var.public_subnets_cidr
-  public_subnets_azs             = var.public_subnets_azs
-  enable_map_public_ip_on_launch = var.enable_map_public_ip_on_launch
-  public_subnets_tags            = var.public_subnets_tags
-
-  ################################################################################
-  # IGW
-  ################################################################################
-
-  create_igw = var.create_igw
-  igw_tags   = var.igw_tags
-
-  ################################################################################
-  # Public RTB
-  ################################################################################
-
-  public_route_table_tags = var.public_route_table_tags
-
-  ################################################################################
-  # Private Subnets
-  ################################################################################
-
-  private_subnets_cidr = var.private_subnets_cidr
-  private_subnets_azs  = var.private_subnets_azs
-  private_subnets_tags = var.private_subnets_tags
-
-  ################################################################################
-  # Elastic IP
-  ################################################################################
-  
-  eip_tags = var.eip_tags
-  
-  ################################################################################
-  # NAT Gateways
-  ################################################################################
-
-  create_nat_gateway = var.create_nat_gateway
-  nat_tags           = var.nat_tags
-
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
 
+# ------------- eks  IAM role -------------------------------
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "eks-cluster-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.example.name
+}
+
+# ---------------------- eks --------------------------------------------------
+
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
+}
+
+# --------------------- node group ----------------------------------
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example"
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids      = aws_subnet.example[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+# ---------- node grp IAM policy --------------------------------
+
+resource "aws_iam_role" "example" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
