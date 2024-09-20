4cb7eb86cdeb628fae6da55e0b865d0acd962893
HarshitDev-Ops <harshit.singh@opstree.com>
2024-09-20 19:47:59 +0530

    updated network

--- aws/network/main.tf
+++ aws/network/main.tf
@@ -2,7 +2,7 @@ locals {
   len_public_subnets  = length(var.public_subnets_cidr)
   len_private_subnets = length(var.private_subnets_cidr)
 
-  # max_subnet_length = max(local.len_private_subnets, local.len_public_subnets, local.len_database_subnets)
+  max_subnet_length = max(local.len_private_subnets, local.len_public_subnets)
 
   create_vpc = var.create_vpc
 }
@@ -11,35 +11,13 @@ locals {
 # VPC
 ################################################################################
 
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "vpc" {
   count                = local.create_vpc ? 1 : 0
   cidr_block           = var.vpc_cidr
   enable_dns_support   = var.vpc_enable_dns_support
   enable_dns_hostnames = var.vpc_enable_dns_hostnames
   tags                 = var.vpc_tags
 }
-
-/*--------------- VPC Peering ---------------*/
-
-resource "aws_vpc_peering_connection" "dev-peer-01" {
-  count = local.create_vpc && var.create_vpc_peering ? 1 : 0
-
-  peer_vpc_id = var.existing_vpc_id
-  vpc_id      = aws_vpc.dev-vpc[0].id
-  auto_accept = true
-  tags        = var.vpc_peering_tags
-}
-
-resource "aws_route" "existing_rtb" {
-  count = local.create_vpc && var.create_vpc_peering ? 1 : 0
-
-  route_table_id            = var.existing_rtb
-  destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.dev-peer-01[0].id
-  # depends_on = [ aws_vpc_peering_connection.dev-peer-01 ]
-}
-
-
 
 ################################################################################
 # Publiс Subnets
@@ -49,10 +27,10 @@ locals {
   create_public_subnets = local.create_vpc && local.len_public_subnets > 0
 }
 
-resource "aws_subnet" "dev-public-subnets" {
+resource "aws_subnet" "public-subnets" {
   count = local.create_public_subnets && (local.len_public_subnets >= length(var.public_subnets_azs)) ? local.len_public_subnets : 0
 
-  vpc_id                  = aws_vpc.dev-vpc[0].id
+  vpc_id                  = aws_vpc.vpc[0].id
   cidr_block              = var.public_subnets_cidr[count.index]
   availability_zone       = var.public_subnets_azs[count.index]
   map_public_ip_on_launch = var.enable_map_public_ip_on_launch
@@ -60,20 +38,20 @@ resource "aws_subnet" "dev-public-subnets" {
 }
 /*--------------- # Internet Gateway ---------------*/
 
-resource "aws_internet_gateway" "dev-igw" {
+resource "aws_internet_gateway" "igw" {
   count = local.create_public_subnets && var.create_igw ? 1 : 0
 
-  vpc_id = aws_vpc.dev-vpc[0].id
+  vpc_id = aws_vpc.vpc[0].id
   tags   = var.igw_tags
 }
 
 
 /*--------------- Public Route Table ---------------*/
 
-resource "aws_route_table" "dev-public-rtb" {
+resource "aws_route_table" "public-rtb" {
   count = local.create_public_subnets ? 1 : 0
 
-  vpc_id = aws_vpc.dev-vpc[0].id
+  vpc_id = aws_vpc.vpc[0].id
   route {
     cidr_block = var.vpc_cidr
     gateway_id = "local"
@@ -81,15 +59,6 @@ resource "aws_route_table" "dev-public-rtb" {
   tags = var.public_route_table_tags
 }
 
-/*--------------- Route VPC Peering Public RTB ---------------*/
-
-resource "aws_route" "public-rtb-vpc-peering" {
-  count = local.create_vpc && var.create_vpc_peering ? 1 : 0
-
-  route_table_id            = element(aws_route_table.dev-public-rtb[*].id, count.index)
-  destination_cidr_block    = "0.0.0.0/0"
-  vpc_peering_connection_id = element(aws_vpc_peering_connection.dev-peer-01[*].id, count.index)
-}
 
 /*--------------- Route IGW Public RTB ---------------*/
 locals {
@@ -99,18 +68,18 @@ locals {
 resource "aws_route" "public-rtb-igw" {
   count = local.create_vpc && local.create_igw ? 1 : 0
 
-  route_table_id         = element(aws_route_table.dev-public-rtb[*].id, count.index)
+  route_table_id         = element(aws_route_table.public-rtb[*].id, count.index)
   destination_cidr_block = var.existing_vpc_cidr
-  gateway_id             = element(aws_internet_gateway.dev-igw[*].id, count.index)
+  gateway_id             = element(aws_internet_gateway.igw[*].id, count.index)
 }
 
 /*--------------- Public RTB + Subnet Association ---------------*/
 
-resource "aws_route_table_association" "dev-public-route-association-01" {
+resource "aws_route_table_association" "public-route-association-01" {
   count = local.create_public_subnets ? local.len_public_subnets : 0
 
-  subnet_id      = element(aws_subnet.dev-public-subnets[*].id, count.index)
-  route_table_id = aws_route_table.dev-public-rtb[0].id
+  subnet_id      = element(aws_subnet.public-subnets[*].id, count.index)
+  route_table_id = aws_route_table.public-rtb[0].id
 }
 
 
@@ -122,10 +91,10 @@ locals {
   create_private_subnets = local.create_vpc && local.len_private_subnets > 0
 }
 
-resource "aws_subnet" "dev-private-subnets" {
+resource "aws_subnet" "private-subnets" {
   count = local.create_private_subnets ? local.len_private_subnets : 0
 
-  vpc_id            = aws_vpc.dev-vpc[0].id
+  vpc_id            = aws_vpc.vpc[0].id
   cidr_block        = var.private_subnets_cidr[count.index]
   availability_zone = var.private_subnets_azs
   tags              = var.private_subnets_tags[count.index]
@@ -133,43 +102,30 @@ resource "aws_subnet" "dev-private-subnets" {
 
 /*--------------- Private RTB ---------------*/
 
-resource "aws_route_table" "dev-private-rtb" {
+resource "aws_route_table" "private-rtb" {
   count = local.create_private_subnets ? 1 : 0
 
-  vpc_id = aws_vpc.dev-vpc[0].id
+  vpc_id = aws_vpc.vpc[0].id
   route {
     cidr_block = var.vpc_cidr
     gateway_id = "local"
   }
-  # route {
-  #   cidr_block                = var.existing_vpc_cidr
-  #   vpc_peering_connection_id = element(aws_vpc_peering_connection.dev-peer-01.*.id, 0)
-  # }
+
   tags = var.private_route_table_tags
-}
-
-/*--------------- Route VPC Peering Private RTB ---------------*/
-
-resource "aws_route" "private-rtb-vpc-peering" {
-  count = local.create_vpc && var.create_vpc_peering ? 1 : 0
-
-  route_table_id            = element(aws_route_table.dev-private-rtb[*].id, 0)
-  destination_cidr_block    = var.existing_vpc_cidr
-  vpc_peering_connection_id = element(aws_vpc_peering_connection.dev-peer-01[*].id, 0)
 }
 
 /*--------------- Private RTB Association ---------------*/
 
-resource "aws_route_table_association" "dev-private-route-association-01" {
+resource "aws_route_table_association" "private-route-association-01" {
   count = local.create_private_subnets ? local.len_private_subnets : 0
 
-  subnet_id      = element(aws_subnet.dev-private-subnets.*.id, count.index)
-  route_table_id = element(aws_route_table.dev-private-rtb.*.id, 0)
+  subnet_id      = element(aws_subnet.private-subnets.*.id, count.index)
+  route_table_id = element(aws_route_table.private-rtb.*.id, 0)
 }
 
 /*--------------- Elastic IP ---------------*/
 
-resource "aws_eip" "dev-elastic-ip" {
+resource "aws_eip" "elastic-ip" {
   count  = local.create_nat_gateway ? 1 : 0
   domain = "vpc"
   tags = var.eip_tags
@@ -180,19 +136,19 @@ locals {
   create_nat_gateway = local.create_private_subnets && var.create_nat_gateway && local.create_igw
 }
 
-resource "aws_nat_gateway" "dev-nat" {
+resource "aws_nat_gateway" "nat" {
   count         = local.create_nat_gateway && local.create_igw ? 1 : 0
-  allocation_id = element(aws_eip.dev-elastic-ip[*].id, 0)
-  subnet_id     = element(aws_subnet.dev-public-subnets[*].id, 0)
+  allocation_id = element(aws_eip.elastic-ip[*].id, 0)
+  subnet_id     = element(aws_subnet.public-subnets[*].id, 0)
   tags          = var.nat_tags
 }
 
-resource "aws_route" "dev-private-nat-gateway" {
+resource "aws_route" "private-nat-gateway" {
   count = local.create_vpc && local.create_nat_gateway ? 1 : 0
 
-  route_table_id         = element(aws_route_table.dev-private-rtb[*].id, count.index)
+  route_table_id         = element(aws_route_table.private-rtb[*].id, count.index)
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.dev-nat[*].id, count.index)
+  nat_gateway_id         = element(aws_nat_gateway.nat[*].id, count.index)
 
 }
 
--- aws/network/output.tf
+++ aws/network/output.tf
@@ -1,11 +1,11 @@
 output "vpc-id" {
-  value = aws_vpc.dev-vpc.*.id
+  value = aws_vpc.vpc.*.id
 }
 
 output "public-subnets-id" {
-  value = aws_subnet.dev-public-subnets.*.id
+  value = aws_subnet.public-subnets.*.id
 }
 
 output "private-subnets-id" {
-  value = aws_subnet.dev-private-subnets.*.id
+  value = aws_subnet.private-subnets.*.id
 }
