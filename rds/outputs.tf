3213d67fd31bf0c4f2469743af03469734f8c959
Bhavnesh Baghel <dazzlingbhavnesh@gmail.com>
2024-09-25 15:06:33 +0530

    rds prev delted

--- rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_instance_endpoint" {
-  description = "The RDS instance endpoint"
-  value       = aws_db_instance.db.endpoint
-}
-
-output "db_instance_id" {
-  description = "The RDS instance ID"
-  value       = aws_db_instance.db.id
-}
