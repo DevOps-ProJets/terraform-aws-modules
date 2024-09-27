c8ff2a0aec8c041b2bbe809f6931d214bdb72625
Bhavnesh Baghel <dazzlingbhavnesh@gmail.com>
2024-09-27 13:57:30 +0530

    enabled csi driver

--- eks/iam.tf
+++ eks/iam.tf
@@ -97,3 +97,50 @@ resource "aws_iam_instance_profile" "node" {
   name = var.name
   role = aws_iam_role.node.name
 }
+
+
+
+# Create the custom policy for EBS CSI Driver
+resource "aws_iam_policy" "AmazonEKS_EBS_CSI_Driver_Policy" {
+  name        = "AmazonEKS_EBS_CSI_Driver_Policy"
+  description = "Custom policy for EBS CSI Driver in EKS"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateVolume",
+          "ec2:AttachVolume",
+          "ec2:DetachVolume",
+          "ec2:DeleteVolume",
+          "ec2:CreateSnapshot",
+          "ec2:DeleteSnapshot",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeInstances",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeVolumeStatus",
+          "ec2:DescribeVolumeAttribute",
+          "ec2:DescribeSnapshotAttribute",
+          "ec2:DescribeInstanceAttribute",
+          "ec2:DescribeInstanceCreditSpecifications",
+          "ec2:DescribeVolumeTypes",
+          "ec2:DescribeVpcAttribute",
+          "ec2:DescribeVpcEndpoints",
+          "ec2:DescribeVpcs",
+          "ec2:ModifyVolume",
+          "ec2:ModifyVolumeAttribute",
+          "ec2:ModifyInstanceAttribute"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the custom policy to the EKS node role
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_EBS_CSI_Driver_Policy" {
+  policy_arn = aws_iam_policy.AmazonEKS_EBS_CSI_Driver_Policy.arn
+  role       = aws_iam_role.node.name
+}
